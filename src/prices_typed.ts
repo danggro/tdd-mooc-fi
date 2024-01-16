a00ed498466623bc061ce02340fb18062abd8c61
danggro <anangdwin55@gmail.com>
2024-01-16 09:30:06 +0700

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -23,13 +23,13 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string);
-    const cost = calculateCost(age, type, convertDateToPlainDate(date), baseCost);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
-  function parseDate(dateString: string | undefined): Date | undefined {
+  function parseDate(dateString: string | undefined): Temporal.PlainDate | undefined {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
