53d83cb5128940e9452f07ad8a2d93fdb60bd427
danggro <anangdwin55@gmail.com>
2024-01-16 08:07:53 +0700

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -1,6 +1,8 @@
 import "./polyfills";
 import express from "express";
 import { Database } from "./database";
+import { Temporal } from "@js-temporal/polyfill";
+import "../src/polyfills";
 
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
